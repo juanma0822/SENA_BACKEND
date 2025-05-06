ac91c9110b8fd8c82a2fc1b175c7dbbb4d863982
juanma0822 <juanmanuelva3243@gmail.com>
2025-05-06 16:47:34 -0500

    feat: add endpoint for registering user entry or exit by guard and update swagger documentation

--- src/Routes/ingresos.routes.js
+++ src/Routes/ingresos.routes.js
@@ -3,6 +3,38 @@ const router = express.Router();
 const IngresosController = require('../controllers/ingresos.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
 
+
+
+/**
+ * @swagger
+ * /api/ingresos/porguarda:
+ *   post:
+ *     summary: Registrar ingreso o salida para un usuario por parte de un guarda
+ *     tags: [Ingresos]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               numero_documento:
+ *                 type: string
+ *                 description: Número de documento del usuario
+ *               tipo_ingreso:
+ *                 type: string
+ *                 enum: [entrada, salida]
+ *                 description: Tipo de ingreso (entrada o salida)
+ *     responses:
+ *       201:
+ *         description: Registro exitoso
+ *       400:
+ *         description: Error en los datos enviados
+ *       500:
+ *         description: Error interno del servidor
+ */
+router.post('/porguarda', verifyToken, IngresosController.registrarIngresoSalidaPorGuarda);
+
 /**
  * @swagger
  * /api/ingresos:
@@ -82,34 +114,5 @@ router.get('/historial-usuario', verifyToken, IngresosController.obtenerIngresos
  */
 router.get('/resumen-dia', verifyToken, IngresosController.resumenDiario);
 
-/**
- * @swagger
- * /api/ingresos/por-guarda:
- *   post:
- *     summary: Registrar ingreso o salida para un usuario por parte de un guarda
- *     tags: [Ingresos]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               numero_documento:
- *                 type: string
- *                 description: Número de documento del usuario
- *               tipo_ingreso:
- *                 type: string
- *                 enum: [entrada, salida]
- *                 description: Tipo de ingreso (entrada o salida)
- *     responses:
- *       201:
- *         description: Registro exitoso
- *       400:
- *         description: Error en los datos enviados
- *       500:
- *         description: Error interno del servidor
- */
-router.post('/por-guarda', verifyToken, IngresosController.registrarIngresoSalidaPorGuarda);
 
 module.exports = router;
